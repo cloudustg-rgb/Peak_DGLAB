d4c83435c4b311d31387d9a466283189633803cc
Dusk-islumi <cloudustg@gmail.com>
2026-01-03 14:11:33 +0800

    Update AssemblyInfo.cs

--- CS2/Properties/AssemblyInfo.cs
+++ CS2/Properties/AssemblyInfo.cs
@@ -1,16 +1,16 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("AliceInCradleXDGLAB")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PeakDGLab")]
+[assembly: AssemblyDescription("AliceInCradle (PEAK) DGLab Integration Plugin")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AliceInCradleXDGLAB")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCompany("PeakDGLab Modding")]
+[assembly: AssemblyProduct("PeakDGLab")]
+[assembly: AssemblyCopyright("Copyright © 2026")] // 已更新为 2026
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("6673ccb9-ecec-4950-a19e-86a6c7a71917")]
+[assembly: Guid("4f239334-5893-4e64-8d82-3a65538f6170")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
@@ -29,5 +29,8 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
+//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
+//通过使用 "*"，如下所示:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
